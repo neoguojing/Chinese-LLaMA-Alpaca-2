3059beecf724995f12fb7aecf18a1f793204ea71
guoerjun <guoerjun@sensetime.com>
2023-09-26 10:47:52 +0800

    up

--- model/chinese/qwen.py
+++ model/chinese/qwen.py
@@ -1,3 +1,3 @@
 from modelscope import snapshot_download
 
-model_dir = snapshot_download("qwen/Qwen-7B", revision = 'v1.1')
+model_dir = snapshot_download("qwen/Qwen-7B", revision = 'v1.1',cache_dir=".")
