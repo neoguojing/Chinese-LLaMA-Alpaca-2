19ac8ee0e49fbcf478ad682426cf51f80a069d44
neoguojing <guojing_neo@163.com>
2023-10-10 20:31:52 +0800

    up

--- model/chinese/qwen.py
+++ model/chinese/qwen.py
@@ -1,3 +1,5 @@
 from modelscope import snapshot_download
 
-model_dir = snapshot_download("qwen/Qwen-7B", revision = 'v1.1')
+# model_dir = snapshot_download("qwen/Qwen-7B", revision = 'v1.1')
+model_dir = snapshot_download("Qwen/Qwen-7B-Chat", revision = 'v1.1')
+print(model_dir)
