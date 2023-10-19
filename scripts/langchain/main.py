72759b41cbd33b5cd180ac56a89429101f5f0ab5
guoerjun <guoerjun@sensetime.com>
2023-10-19 18:07:24 +0800

    up

--- scripts/langchain/main.py
+++ scripts/langchain/main.py
@@ -1,21 +1,31 @@
-import argparse
+
 import os
-parser = argparse.ArgumentParser()
-parser.add_argument('--file_path', required=True, type=str)
-parser.add_argument('--embedding_path', required=True, type=str)
-parser.add_argument('--model_path', required=True, type=str)
-parser.add_argument('--gpu_id', default="0", type=str)
-parser.add_argument('--chain_type', default="refine", type=str)
-args = parser.parse_args()
 
-os.environ['OPENAI_API_KEY'] = 'value'
-
-from langchain.chat_models import ChatOpenAI
+os.environ['OPENAI_API_KEY'] = ''
+from langchain import OpenAI
+from langchain.chains.summarize import load_summarize_chain
+from langchain.chains import AnalyzeDocumentChain
+from langchain.prompts import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 if __name__ == '__main__':
-    print("start")
-    llm = ChatOpenAI(
-        model="gpt-3.5-turbo-16k",
-        streaming=True,
-        temperature=0,
+    prompt_template = """Write a concise summary of the following:
+
+
+    {text}
+
+
+    CONCISE SUMMARY IN INDONESIAN:"""
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
+
+    with open("/content/article.txt") as f:
+        article_kompas = f.read()
+
+    llm = OpenAI(temperature=0)
+    summary_chain = load_summarize_chain(llm, chain_type="map_reduce", map_prompt=PROMPT, combine_prompt=PROMPT)
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=0
     )
+    summarize_document_chain = AnalyzeDocumentChain(combine_docs_chain=summary_chain,text_splitter=text_splitter)
+    summarize_document_chain.run(article_kompas)
