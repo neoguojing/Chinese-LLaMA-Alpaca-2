04090b1821644dc859ab8642da809ae7eed9f3f4
neoguojing <guojing_neo@163.com>
2023-10-20 22:21:25 +0800

    add longchain

--- scripts/langchain/main.py
+++ scripts/langchain/main.py
@@ -1,31 +1,34 @@
 
 import os
 
-os.environ['OPENAI_API_KEY'] = ''
-from langchain import OpenAI
+os.environ['OPENAI_API_KEY'] = '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 '
+from langchain.llms import OpenAI
 from langchain.chains.summarize import load_summarize_chain
 from langchain.chains import AnalyzeDocumentChain
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import DirectoryLoader,TextLoader
 
 if __name__ == '__main__':
-    prompt_template = """Write a concise summary of the following:
+    prompt_template = """解析以下文件内容：
 
 
     {text}
 
 
-    CONCISE SUMMARY IN INDONESIAN:"""
+    以问答的形式输出解"""
     PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
 
-    with open("/content/article.txt") as f:
-        article_kompas = f.read()
-
+    loader = DirectoryLoader('../../dataset/generate/', glob="**/*.txt",loader_cls=TextLoader)
+    docs = loader.load()
+    # print("**************",docs[0])
+    
     llm = OpenAI(temperature=0)
+
     summary_chain = load_summarize_chain(llm, chain_type="map_reduce", map_prompt=PROMPT, combine_prompt=PROMPT)
 
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, chunk_overlap=0
+        chunk_size=500, chunk_overlap=0
     )
     summarize_document_chain = AnalyzeDocumentChain(combine_docs_chain=summary_chain,text_splitter=text_splitter)
-    summarize_document_chain.run(article_kompas)
+    summarize_document_chain.run(docs[0].page_content)
