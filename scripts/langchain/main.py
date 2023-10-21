44cd44c1df40d7a09b94f462a1b73903c6b9cca3
neoguojing <guojing_neo@163.com>
2023-10-21 18:57:24 +0800

    up

--- scripts/langchain/main.py
+++ scripts/langchain/main.py
@@ -5,21 +5,17 @@ from langchain.chains import AnalyzeDocumentChain
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import DirectoryLoader,TextLoader
+from langchain.chains.question_answering import load_qa_chain
 from model_factory import  ModelFactory
 import textwrap
 verbose = True
 if __name__ == '__main__':
-    #map
-    map_template = """以下是原始文本内容：
-    {text}
-    以问答的形式输出解析结果"""
-    map_prompt = PromptTemplate(template=map_template,input_variables=["text"])
-
-    # Reduce
-    reduce_template = """依据以下问答对:
-    {text}
-    将问答内容去重"""
-    reduce_prompt = PromptTemplate.from_template(reduce_template)
+ 
+    prompt_str = """
+        将如下文字转化为问答： 
+        {text}
+    """
+    prompt = PromptTemplate.from_template(prompt_str)
 
     # loader = DirectoryLoader('../../dataset/generate/', glob="**/*.txt",loader_cls=TextLoader)
     loader = TextLoader("./doc.txt")
@@ -27,17 +23,20 @@ if __name__ == '__main__':
     # print("**************",docs[0])
     
     # llm = ModelFactory().get_model("openai")
-    llm = ModelFactory().get_model("llama")
+    llm = ModelFactory().get_model("qwen")
     
-
-    summary_chain = load_summarize_chain(llm, chain_type="map_reduce", map_prompt=map_prompt, 
-                                         combine_prompt=reduce_prompt,verbose=verbose)
-
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=500, chunk_overlap=0
     )
-    summarize_document_chain = AnalyzeDocumentChain(combine_docs_chain=summary_chain,
-                                                    text_splitter=text_splitter)
-    # docs[0].page_content
-    qa = summarize_document_chain.run(docs[0].page_content)
-    print(f"qa: {textwrap.fill(qa, width=100)}")
+    texts = []
+    for doc in docs:
+        # print(doc.page_content)
+        text = doc.page_content
+        texts += text_splitter.create_documents([text])
+
+    chain = prompt | llm 
+    
+    for text in texts:
+        print(text)
+        answer = chain.invoke({"text": text})
+        print(f"answer: {textwrap.fill(answer, width=100)}")
