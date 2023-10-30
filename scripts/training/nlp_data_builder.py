90aa7b2a31675d5498dc49d40ab75e13a2794d21
guoerjun <guoerjun@sensetime.com>
2023-10-30 18:44:25 +0800

    up

--- scripts/training/nlp_data_builder.py
+++ scripts/training/nlp_data_builder.py
@@ -90,47 +90,51 @@ def generate_tokenize_func(tokenizer: PreTrainedTokenizer,
             pdb.set_trace()
             print("qwen:",examples)
             roles = {"user": "<|im_start|>user", "assistant": "<|im_start|>assistant"}
-            im_start = tokenizer.im_start_id
-            im_end = tokenizer.im_end_id
-            nl_tokens = tokenizer('\n').input_ids
-            _system = tokenizer('system').input_ids + nl_tokens
-            _user = tokenizer('user').input_ids + nl_tokens
-            _assistant = tokenizer('assistant').input_ids + nl_tokens
+            im_start = tokenizer.im_start_id #151644
+            im_end = tokenizer.im_end_id #151645
+            nl_tokens = tokenizer('\n').input_ids #[198]
+            _system = tokenizer('system').input_ids + nl_tokens #[8948, 198]
+            _user = tokenizer('user').input_ids + nl_tokens #[872, 198]
+            _assistant = tokenizer('assistant').input_ids + nl_tokens #[77091, 198]
 
             # Apply prompt templates
             # sources = examples["conversations"]
             input_ids, targets = [], []
-            for _from, _value in zip(examples['from'],examples['value']):
-                print("from:",_from)
-                print("value:",_value)
-            #     if roles[source[0]["from"]] != roles["user"]:
-            #         source = source[1:]
 
-            #     input_id, target = [], []
-            #     system = [im_start] + _system + tokenizer(system_message).input_ids + [im_end] + nl_tokens
-            #     input_id += system
-            #     target += [im_start] + [IGNORE_TOKEN_ID] * (len(system)-3) + [im_end] + nl_tokens
-            #     assert len(input_id) == len(target)
-            #     for j, sentence in enumerate(source):
-            #         role = roles[sentence["from"]]
-            #         _input_id = tokenizer(role).input_ids + nl_tokens + \
-            #             tokenizer(sentence["value"]).input_ids + [im_end] + nl_tokens
-            #         input_id += _input_id
-            #         if role == '<|im_start|>user':
-            #             _target = [im_start] + [IGNORE_TOKEN_ID] * (len(_input_id)-3) + [im_end] + nl_tokens
-            #         elif role == '<|im_start|>assistant':
-            #             _target = [im_start] + [IGNORE_TOKEN_ID] * len(tokenizer(role).input_ids) + \
-            #                 _input_id[len(tokenizer(role).input_ids)+1:-2] + [im_end] + nl_tokens
-            #         else:
-            #             raise NotImplementedError
-            #         target += _target
-            #     assert len(input_id) == len(target)
-            #     input_id += [tokenizer.pad_token_id] * (max_seq_length - len(input_id))
-            #     target += [IGNORE_TOKEN_ID] * (max_seq_length - len(target))
-            #     input_ids.append(input_id[:max_seq_length])
-            #     targets.append(target[:max_seq_length])
-            # input_ids = torch.tensor(input_ids, dtype=torch.int)
-            # targets = torch.tensor(targets, dtype=torch.int)
+            input_id, target = [], []
+            system = [im_start] + _system + tokenizer(system_message).input_ids + [im_end] + nl_tokens
+            input_id += system
+            target += [im_start] + [IGNORE_TOKEN_ID] * (len(system)-3) + [im_end] + nl_tokens
+            assert len(input_id) == len(target)
+            for idx, (_from, _value) in enumerate(zip(examples['from'],examples['value'])):
+                print(f"From: {_from}, Value: {_value}")
+                
+                if idx == 0 and roles[_from] != roles["user"]:
+                    continue
+
+                role = roles[_from]
+                
+                _input_id = tokenizer(role).input_ids + nl_tokens + \
+                    tokenizer(_value).input_ids + [im_end] + nl_tokens
+                input_id += _input_id
+                if role == '<|im_start|>user':
+                    _target = [im_start] + [IGNORE_TOKEN_ID] * (len(_input_id)-3) + [im_end] + nl_tokens
+                elif role == '<|im_start|>assistant':
+                    _target = [im_start] + [IGNORE_TOKEN_ID] * len(tokenizer(role).input_ids) + \
+                        _input_id[len(tokenizer(role).input_ids)+1:-2] + [im_end] + nl_tokens
+                else:
+                    raise NotImplementedError
+                target += _target
+
+
+            assert len(input_id) == len(target)
+            input_id += [tokenizer.pad_token_id] * (max_seq_length - len(input_id))
+            target += [IGNORE_TOKEN_ID] * (max_seq_length - len(target))
+            input_ids.append(input_id[:max_seq_length])
+            targets.append(target[:max_seq_length])
+
+            input_ids = torch.tensor(input_ids, dtype=torch.int)
+            targets = torch.tensor(targets, dtype=torch.int)
 
             return dict(
                 input_ids=input_ids,
