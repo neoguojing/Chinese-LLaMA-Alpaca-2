7c7dc0b1e59c5bd87535ef130043d153f58ef1c6
neoguojing <guojing_neo@163.com>
2023-11-11 20:32:18 +0800

    up

--- dataset/chat/README.md
+++ dataset/chat/README.md
@@ -1,1 +1,4 @@
 # format
+
+## pdf 转换txt，保留表格格式
+> pdftotext -layout ir2023_ashare.pdf 
---
+++ scripts/training/reward.py
@@ -0,0 +1,24 @@
+
+from transformers import (
+    CONFIG_MAPPING,
+    MODEL_FOR_CAUSAL_LM_MAPPING,
+    AutoConfig,
+    AutoModelForCausalLM,
+    LlamaForCausalLM,
+    LlamaTokenizer,
+    AutoTokenizer,
+    HfArgumentParser,
+    Trainer,
+    TrainingArguments,
+    is_torch_tpu_available,
+    set_seed,
+)
+
+class RewardTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        rewards_j = model(input_ids=inputs["input_ids_j"], attention_mask=inputs["attention_mask_j"])[0]
+        rewards_k = model(input_ids=inputs["input_ids_k"], attention_mask=inputs["attention_mask_k"])[0]
+        loss = -nn.functional.logsigmoid(rewards_j - rewards_k).mean()
+        if return_outputs:
+            return loss, {"rewards_j": rewards_j, "rewards_k": rewards_k}
+        return loss
