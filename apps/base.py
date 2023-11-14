72f857b31f5950601af0706a278c8139d766899c
guoerjun <guoerjun@sensetime.com>
2023-11-14 17:08:00 +0800

    up

--- apps/base.py
+++ apps/base.py
@@ -2,7 +2,7 @@ import abc
 import asyncio
 import torch
 from langchain.llms.base import LLM
-
+from pydantic import  Field
 
 class ITask(abc.ABC):
     
@@ -23,6 +23,7 @@ class Task(ITask):
 
 
 class CustomerLLM(LLM):
+    device: str = Field(torch.device('cpu'))
     def __init__(self,**kwargs):
         super(CustomerLLM, self).__init__()
         if torch.cuda.is_available():
