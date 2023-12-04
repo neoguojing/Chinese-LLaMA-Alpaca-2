259f15262bbf1c5671d1174f30f4684a55cfb414
guoerjun <guoerjun@sensetime.com>
2023-12-04 16:31:53 +0800

    use stabilityai/sdxl-turbo

--- apps/image/sd.py
+++ apps/image/sd.py
@@ -11,6 +11,8 @@ top_package_path = os.path.abspath(os.path.join(current_dir, "../../"))
 sys.path.insert(0, top_package_path)
 from diffusers import DiffusionPipeline,DPMSolverMultistepScheduler
 from diffusers import LCMScheduler, AutoPipelineForText2Image
+from diffusers import AutoPipelineForImage2Image
+from diffusers.utils import load_image
 import torch
 from langchain.llms.base import LLM
 from typing import Any, List, Mapping, Optional,Union
@@ -40,31 +42,42 @@ class StableDiff(CustomerLLM):
     high_noise_frac: float = 0.8
     file_path: str = "./"
 
+    # def __init__(self, model_path: str=os.path.join(model_root,"stable-diffusion"),**kwargs):
+    #     super(StableDiff, self).__init__(
+    #         llm=DiffusionPipeline.from_pretrained(
+    #             model_path, torch_dtype=torch.float16, variant="fp16", use_safetensors=True,
+    #     ))
+    #     self.model_path = model_path
+    #     # self.model = DiffusionPipeline.from_pretrained(
+    #     #     "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True,
+    #     #     cache_dir=os.path.join(model_root,"stable-diffusion")
+    #     # )
+    #     # self.model.save_pretrained(os.path.join(model_root,"stable-diffusion"))
+        
+    #     # self.model = AutoPipelineForText2Image.from_pretrained(model_path, torch_dtype=torch.float16, variant="fp16")
+    #     # self.model.scheduler = LCMScheduler.from_config(self.model.scheduler.config)
+    #     # self.model.enable_attention_slicing()
+    #     # 推理速度变慢
+    #     # self.model.unet = torch.compile(self.model.unet, mode="reduce-overhead", fullgraph=True)
+    #     # self.model.to(self.device)
+    #     # 使用cpu和to('cuda')互斥，内存减小一半
+    #     self.model.enable_model_cpu_offload()
+    #     # 加速
+    #     # adapter_id = "latent-consistency/lcm-lora-sdxl"
+    #     # self.model.load_lora_weights(adapter_id)
+    #     # self.model.fuse_lora()
+    #     # self.model.save_lora_weights(os.path.join(model_root,"stable-diffusion"),unet_lora_layers)
+
     def __init__(self, model_path: str=os.path.join(model_root,"stable-diffusion"),**kwargs):
         super(StableDiff, self).__init__(
-            llm=DiffusionPipeline.from_pretrained(
-                model_path, torch_dtype=torch.float16, variant="fp16", use_safetensors=True,
+            llm=AutoPipelineForText2Image.from_pretrained(
+                os.path.join(model_root,"sdxl-turbo"), torch_dtype=torch.float16, variant="fp16"
         ))
         self.model_path = model_path
-        # self.model = DiffusionPipeline.from_pretrained(
-        #     "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True,
-        #     cache_dir=os.path.join(model_root,"stable-diffusion")
-        # )
-        # self.model.save_pretrained(os.path.join(model_root,"stable-diffusion"))
-        
-        # self.model = AutoPipelineForText2Image.from_pretrained(model_path, torch_dtype=torch.float16, variant="fp16")
-        # self.model.scheduler = LCMScheduler.from_config(self.model.scheduler.config)
-        # self.model.enable_attention_slicing()
-        # 推理速度变慢
-        # self.model.unet = torch.compile(self.model.unet, mode="reduce-overhead", fullgraph=True)
-        # self.model.to(self.device)
+        self.model.to(self.device)
         # 使用cpu和to('cuda')互斥，内存减小一半
         self.model.enable_model_cpu_offload()
-        # 加速
-        # adapter_id = "latent-consistency/lcm-lora-sdxl"
-        # self.model.load_lora_weights(adapter_id)
-        # self.model.fuse_lora()
-        # self.model.save_lora_weights(os.path.join(model_root,"stable-diffusion"),unet_lora_layers)
+
 
     @property
     def _llm_type(self) -> str:
@@ -82,9 +95,11 @@ class StableDiff(CustomerLLM):
         **kwargs: Any,
     ) -> str:
 
-        image = self.model(
-            **self.get_inputs(prompt)
-        ).images[0]
+        # image = self.model(
+        #     **self.get_inputs(prompt)
+        # ).images[0]
+
+        image = self.model(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
 
         file_name = calculate_md5(prompt)+".png"
         path = os.path.join(self.file_path, file_name)
@@ -103,8 +118,7 @@ class StableDiff(CustomerLLM):
         """Get the identifying parameters."""
         return {"model_path": self.model_path}
     
-from diffusers import AutoPipelineForImage2Image
-from diffusers.utils import load_image
+
 class Image2Image(CustomerLLM):
     model_path: str = Field(None, alias='model_path')
     refiner: Any = None
@@ -116,9 +130,9 @@ class Image2Image(CustomerLLM):
     def __init__(self, model_path: str=os.path.join(model_root,"sdxl-turbo"),**kwargs):
         super(Image2Image, self).__init__(
             llm=AutoPipelineForImage2Image.from_pretrained(
-                "stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16"
+                os.path.join(model_root,"sdxl-turbo"), torch_dtype=torch.float16, variant="fp16"
         ))
-        self.model.save_pretrained(os.path.join(model_root,"sdxl-turbo"))
+        # self.model.save_pretrained(os.path.join(model_root,"sdxl-turbo"))
         self.model_path = model_path
 
     @property
